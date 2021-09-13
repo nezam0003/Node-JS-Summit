3474a4379af684619bfb34c17c6e09d037802c0c
nezam0003 <fhameedk.nezam@gmail.com>
2021-09-13 17:22:13 +0600

    express() - all function done

--- index.js
+++ index.js
@@ -5,13 +5,24 @@ const app = express();
 
 // app.use(express.json());
 // app.use(express.raw());
-app.use(express.text());
+// app.use(express.text())
+// app.use(express.urlencoded());
+// app.use(
+//   express.static(`${__dirname}/public/`, {
+//     index: "home.html",
+//   })
+// );
 
-app.get("/", (req, res) => {
-  res.send("this is home page with get method");
+const router = express.Router({
+  caseSensitive: true,
 });
-app.post("/", (req, res) => {
-  console.log(req.body);
+app.use(router);
+
+router.get("/contact", (req, res) => {
+  res.send("this is contact page with get method");
+});
+router.post("/", (req, res) => {
+  // console.log(req.body);
   res.send("this is home page with post method");
 });
 
