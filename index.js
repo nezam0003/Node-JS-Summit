ce406f05ce0f8ff33757f3764dbee8d8dee6783c
nezam0003 <fhameedk.nezam@gmail.com>
2021-09-13 15:55:45 +0600

    express - text()and raw()

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
+// app.use(express.raw());
+app.use(express.text());
 
 app.get("/", (req, res) => {
   res.send("this is home page with get method");
 });
 app.post("/", (req, res) => {
-  console.log(req.body.name);
+  console.log(req.body);
   res.send("this is home page with post method");
 });
 
