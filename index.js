09fd691e384e960a57f8b8622acb61258487923d
nezam0003 <fhameedk.nezam@gmail.com>
2021-09-13 15:33:39 +0600

    expressJS - express.json() done

---
+++ index.js
@@ -0,0 +1,18 @@
+// Dependencies
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("this is home page with get method");
+});
+app.post("/", (req, res) => {
+  console.log(req.body.name);
+  res.send("this is home page with post method");
+});
+
+app.listen(4000, () => {
+  console.log(`Listing on Port no 4000`);
+});
