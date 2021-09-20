1a11c1ea3b7604fdd034063626517fa57593287b
nezam0003 <fhameedk.nezam@gmail.com>
2021-09-21 00:20:01 +0600

    mongoose

---
+++ express/26-mongoos/index.js
@@ -0,0 +1,30 @@
+// dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const todoHandler = require("./route-handler/todoHandler");
+
+const app = express();
+app.use(express.json());
+
+// database connection with mongoose
+mongoose
+  .connect("mongodb://localhost/todos", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connection successfull"))
+  .catch((err) => console.log("can not connect with database"));
+
+// Application routes
+app.use("/todo", todoHandler);
+
+// default error handler
+const errorHandler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  } else {
+    res.status(500).json({ error: err });
+  }
+};
+
+app.listen(5000, () => console.log("server runing on port 5000"));
